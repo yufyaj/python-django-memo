2d19ad427a4f56b72a5d663dbf6303efd159c3de
yuyaK <yk1994k@gmail.com>
2023-07-05 00:20:44 +0900

    model追加完了

--- code/todo/models.py
+++ code/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField("タスク名", max_length=20)
+    description = models.TextField("詳細", blank=True)
+    status = models.BooleanField("状態")
+
+    def __str__(self):
+        return self.title
