856ac80a97307d3e661101670321450b8197b945
yuyaK <yk1994k@gmail.com>
2023-07-05 23:57:01 +0900

    /todoにdb値を表示

--- code/todo/urls.py
+++ code/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import TodoList
 
-app_name = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('todo', TodoList.as_view(), name='list')
 ]
--- code/todo/views.py
+++ code/todo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Todo
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+class TodoList(ListView):
+    model = Todo
+    context_object_name = 'tasks'
